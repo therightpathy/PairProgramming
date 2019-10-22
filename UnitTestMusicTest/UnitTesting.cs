645e1e5e9a6ebe3b20ce36dbb21cb18ba97b1063
therightpathy <p_razmussen@hotmail.com>
2019-10-22 10:15:15 +0200

    Initializing UnitTesting
    Started doing UnitTesting

---
+++ UnitTestMusicTest/UnitTesting.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PairProgramming;
+
+namespace UnitTestMusicTest
+{
+    [TestClass]
+    public class UnitTesting
+    {
+        private Music _music;
+
+        //Initializing test testing all methods listed below 
+        [TestInitialize]
+        public void BeforeTest()
+        {
+            _music = new Music("Rave", "Rusky", 2.50, 1992, "Universal", "Rock");
+        }
+
+        [TestMethod]
+        public void TestTitle()
+        {
+            Assert.AreEqual("Rave", _music.Title);
+            _music.Title = "Reverend";
+            Assert.AreEqual("Reverend", _music.Title);
+            try
+            {
+                _music.Title = "J";
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual("Title is shorter than two characters", e.Message);
+            }
+        }
+
+    }
+}
