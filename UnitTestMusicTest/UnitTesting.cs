72c70e4c13bc1b41b126c677824ffaf08719bc04
therightpathy <p_razmussen@hotmail.com>
2019-10-22 11:13:21 +0200

    So far unit testing

--- UnitTestMusicTest/UnitTesting.cs
+++ UnitTestMusicTest/UnitTesting.cs
@@ -26,13 +26,64 @@ namespace UnitTestMusicTest
             Assert.AreEqual("Reverend", _music.Title);
             try
             {
-                _music.Title = "J";
+                _music.Title = null;
+                Assert.Fail();
             }
-            catch (Exception e)
+            catch (ArgumentException e)
             {
-                Assert.AreEqual("Title is shorter than two characters", e.Message);
+                Assert.AreEqual("Title skal være længere end 0 tegn og være Gyldig", e.Message);
             }
         }
 
+        [TestMethod]
+        public void TestArtist()
+        {
+            Assert.AreEqual("Rusky", _music.Artist);
+            _music.Artist = "Riskian";
+            Assert.AreEqual("Riskian", _music.Artist);
+            try
+            {
+                _music.Artist = null;
+                Assert.Fail();
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual("Artist skal være længere end 2 tegn og være Gyldig", e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void TestDuration()
+        {
+            Assert.AreEqual(2.14, _music.Duration);
+            _music.Duration = 3.65;
+            Assert.AreEqual(3.65, _music.Duration);
+            try
+            {
+                _music.Duration = 1001;
+                Assert.Fail();
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual("Duration skal være mellem 0 og 1000", e.Message);
+            }
+        }
+
+        [TestMethod]
+        public void TestYearOfPub()
+        {
+            Assert.AreEqual(1991, _music.YearOfPub);
+            _music.YearOfPub = 1995;
+            Assert.AreEqual(1995, _music.YearOfPub);
+            try
+            {
+                _music.Duration = 1849;
+                Assert.Fail();
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual("Music kan kun tilføjes efter det er udgivet", e.Message);
+            }
+        }
     }
 }
