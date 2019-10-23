e33ed1e74ac7ac63e870c8bde7a16e9a93d279b5
CBoens <43374935+CBoens@users.noreply.github.com>
2019-10-23 11:36:02 +0200

    SQL code

--- PairProgramming/Music.cs
+++ PairProgramming/Music.cs
@@ -158,5 +158,23 @@ namespace PairProgramming
             }
         }
         */
+
+
+
+        /*
+        CREATE TABLE PairMusik(
+
+	        Title VARCHAR(80) NOT NULL,
+	        Artist VARCHAR(80) NOT NULL,
+	        Dura FLOAT NOT NULL,
+	        YearOfPub int NOT NULL,
+	        Label VARCHAR(80) NOT NULL,
+	        Genre VARCHAR(80) NOT NULL,
+
+
+	        Primary KEY (Title, Artist)
+        );
+         
+         */
     }
 }
