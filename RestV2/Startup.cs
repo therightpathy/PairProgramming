54cbc95794cfca7ada9e4bc3e767afda336054fa
therightpathy <p_razmussen@hotmail.com>
2019-10-30 10:54:03 +0100

    Fat noget CORS

--- RestV2/Startup.cs
+++ RestV2/Startup.cs
@@ -25,6 +25,7 @@ namespace RestV2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +36,12 @@ namespace RestV2
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(
+                options =>
+                {
+                    options.AllowAnyOrigin().AllowAnyMethod();
+                    //allowing anything from anywhere.
+                });
             app.UseMvc();
         }
     }
